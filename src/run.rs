3d02704acc9406e6494499bd73fa330b6ecb0903
tomo1227 <tomo.soccer1227@gmail.com>
2025-01-12 10:55:03 +0000

    stringじゃなくてpathのvecにする

--- src/run.rs
+++ src/run.rs
@@ -1,12 +1,13 @@
 use std::collections::HashMap;
+use std::path::PathBuf;
 
 use clap::Parser;
 use walkdir::WalkDir;
 
 #[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
 struct Cli {
-    pattern: String,
-    dirs: Vec<std::path::PathBuf>,
+    dirs: Vec<PathBuf>,
     #[arg(short, long)]
     verbose: bool,
     #[arg(short, long)]
@@ -16,28 +17,27 @@ struct Cli {
 pub fn run(_args: std::env::ArgsOs) -> Result<(), i32> {
     let cli = Cli::parse();
 
-    let mut file_map: HashMap<String, Vec<String>> = HashMap::new();
-    for dir in cli.dirs {
+    let mut file_map: HashMap<String, Vec<PathBuf>> = HashMap::new();
+    for dir in cli.dirs.clone() {
+        // TODO:fn dir内のすべてのファイルを抽出
+        // 同じものをhashmapに入れる
         for entry in WalkDir::new(&dir).into_iter().filter_map(Result::ok) {
             if entry.file_type().is_file() {
                 let file_name = entry.file_name().to_string_lossy().into_owned();
                 file_map
                     .entry(file_name)
                     .or_default()
-                    .push(entry.path().to_string_lossy().into_owned());
+                    .push(entry.path().to_path_buf());
             }
         }
     }
     for (file_name, paths) in file_map {
         if paths.len() > 1 {
-            println!("File: {}", file_name);
+            println!("重複ファイル: {}", file_name);
             for path in paths {
-                println!("  {}", path);
+                println!("  {}", path.display());
             }
         }
     }
-    println!("pattern: {:?}", cli.pattern);
-    // println!("path: {:?}", cli.dirs);
-    println!("verbose mode: {:?}", cli.verbose);
     Ok(())
 }
